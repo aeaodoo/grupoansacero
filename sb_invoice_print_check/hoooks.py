07035407e4e81d06d399f293585a3e99a3d5e5a1
Josué <josueflores.05@gmail.com>
2023-01-04 06:29:00 -0600

    [FIX] search attachment invoice only pdf

--- sb_invoice_print_check/hoooks.py
+++ sb_invoice_print_check/hoooks.py
@@ -6,7 +6,12 @@ _logger = logging.getLogger(__name__)
 
 def _verify_invoice_attachment(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
+    attachments = env['ir.attachment']
+    
     # Search invoice
     invoices_partner = env['account.move'].search([('move_type','=','out_invoice')])
     for invoice in invoices_partner.filtered(lambda inv: inv.message_attachment_count > 0):
-        invoice.printed = True
+        count = attachments.search([('res_model','=','account.move'), ('res_id','=',invoice.id),
+                                        ('name','ilike',"%.pdf")], count=True) # Only PDF files.
+        if count > 0:
+            invoice.printed = True
